054c754508a75000c001f8ddda9da44885cff855
Lukasz Hawro <lhawro@outlook.com>
2015-06-09 22:27:26 +0100

    load pins chronologically

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require jquery.turbolinks
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
 //= require isotope/jquery.isotope
