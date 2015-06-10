8d42328bff5b354fc98551d0c2a64ec01b9e4e5c
Lukasz Hawro <lhawro@outlook.com>
2015-06-10 22:46:32 +0100

    user name added

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,5 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-
+	validates :image, presence: true
+	validates :description, presence: true
 end
